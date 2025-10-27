b0716f33212548cfa5550533b30e345cab6cc56e
Dipu Debnath <dipudebnath966@gmail.com>
2025-10-27 15:39:14 +0600

    add const folder

---
+++ src/app/const/Roles.ts
@@ -0,0 +1,10 @@
+export default {
+  ADMIN: 'admin',
+  USER: 'user',
+  PROVIDER: 'provider',
+  USER_PROVIDER_COMMON: 'userProviderCommon',
+  COMMON_ADMIN_PROVIDER: 'commonAdminProvider',
+  SUPER_ADMIN: 'superAdmin',
+  COMMON: 'common',
+  COMMON_ADMIN: 'commonAdmin',
+};
